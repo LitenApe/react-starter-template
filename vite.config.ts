1615d881c87898a7c89cb46f70e88c3a84b3af06
Son Thanh Vo <sonvo95@outlook.com>
2023-08-10 19:52:09 +0200

    test: run tests in browser env

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    browser: {
+      enabled: true,
+      name: 'chrome',
+      headless: true,
+    },
+  },
   resolve: {
     alias: {
       '~': '/src',
