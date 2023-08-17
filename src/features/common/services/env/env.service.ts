09cc216226ef114de36fdd2c92ed3ecf4a1e5e98
Son Thanh Vo <sonvo95@outlook.com>
2023-08-17 20:49:29 +0200

    feat: expand environment variables

--- src/features/common/services/env/env.service.ts
+++ src/features/common/services/env/env.service.ts
@@ -3,4 +3,7 @@ export const Environment = {
   OFFLINE: import.meta.env.VITE_OFFLINE === 'true',
   OFFLINE_PREFIX: import.meta.env.VITE_OFFLINE_PREFIX,
   OFFLINE_HEADER: import.meta.env.VITE_OFFLINE_HEADER,
+  DEV: import.meta.env.DEV,
+  PROD: import.meta.env.PROD,
+  MODE: import.meta.env.MODE,
 };
