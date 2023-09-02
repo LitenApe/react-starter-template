6f2ff0da461d8d5425a5699dc4bfd43b6d0e4876
Son Thanh Vo <sonvo95@outlook.com>
2023-09-02 21:02:06 +0200

    test: use importMock instead of importActual

--- test/setup-mocks.ts
+++ test/setup-mocks.ts
@@ -1,5 +1,5 @@
 import { vi } from 'vitest';
 
 vi.mock('~/features/common/services/configuration', async () =>
-  vi.importActual('~/features/common/services/configuration/__mocks__'),
+  vi.importMock('~/features/common/services/configuration/__mocks__'),
 );
