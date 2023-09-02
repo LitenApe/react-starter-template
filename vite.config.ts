5ddd69e5a1257933775dc9c8224f0ecfe6090f29
Son Thanh Vo <sonvo95@outlook.com>
2023-09-02 20:03:31 +0200

    chore: exclude mocks from coverage report

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,27 @@ export default defineConfig({
     setupFiles: ['./test/setup-mocks.ts'],
     reporters: ['verbose'],
     environment: 'jsdom',
+    coverage: {
+      provider: 'v8',
+      exclude: [
+        'coverage/**',
+        'dist/**',
+        'packages/*/test?(s)/**',
+        '**/*.d.ts',
+        '**/virtual:*',
+        '**/__x00__*',
+        '**/\x00*',
+        'cypress/**',
+        'test?(s)/**',
+        'test?(-*).?(c|m)[jt]s?(x)',
+        '**/*{.,-}{test,spec}.?(c|m)[jt]s?(x)',
+        '**/__tests__/**',
+        '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+        '**/vitest.{workspace,projects}.[jt]s?(on)',
+        '**/.{eslint,mocha,prettier}rc.{?(c|m)js,yml}',
+        '**/__mocks__/**',
+      ],
+    },
   },
   server: {
     proxy: {
